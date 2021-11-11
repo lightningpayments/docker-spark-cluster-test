731d83513dd5b3c1cf6396f487ef92abe7417efd
ronakkany <ronny.wels@gmx.de>
2021-11-11 17:46:21 +0100

    update k8s files

--- k8s/build-images.sh
+++ k8s/build-images.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+minikube delete && minikube start --driver=hyperkit
+eval $(minikube docker-env)
 docker build --no-cache --tag docker-spark-test-app:3.0.0 -f docker-spark-test-app:3.0.0.Dockerfile .
 docker build --no-cache --tag spark-base:3.1.2 -f spark-base:3.1.2.Dockerfile .
 docker build --no-cache --tag spark-master:3.1.2 -f spark-master:3.1.2.Dockerfile .
