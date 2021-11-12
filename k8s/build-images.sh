14a47ca8cae4e8933906fca5a83a70702a4a0f0c
ronakkany <ronny.wels@gmx.de>
2021-11-12 16:36:53 +0100

    update

--- k8s/build-images.sh
+++ k8s/build-images.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-minikube delete && minikube start --driver=hyperkit
+minikube delete
+minikube start --driver=hyperkit
 eval $(minikube docker-env)
+
 docker build --no-cache --tag docker-spark-test-app:3.0.0 -f docker-spark-test-app:3.0.0.Dockerfile .
-docker build --no-cache --tag spark-base:3.1.2 -f spark-base:3.1.2.Dockerfile .
-docker build --no-cache --tag spark-master:3.1.2 -f spark-master:3.1.2.Dockerfile .
-docker build --no-cache --tag spark-worker:3.1.2 -f spark-worker:3.1.2.Dockerfile .
+docker build --tag spark-base:3.1.2 -f spark-base:3.1.2.Dockerfile .
+docker build --tag spark-master:3.1.2 -f spark-master:3.1.2.Dockerfile .
+docker build --tag spark-worker:3.1.2 -f spark-worker:3.1.2.Dockerfile .
 
